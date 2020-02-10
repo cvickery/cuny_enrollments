58b4f6f069805039af85e76b1fdd135364ca76ec
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-02-10 11:43:37 -0500

    Update mogrify.py
    Add option to list meeting days in separate columns.

--- mogrify.py
+++ mogrify.py
@@ -78,12 +78,14 @@ def make_meetings_str(start, end, days_yn):
   for i in range(len(days_yn)):
     if days_yn[i] == 'Y':
       day_list.add(i)
-  days_str = ',@'.join([days[index] for index in sorted(day_list)])
-  return (f'{start.replace(":00.000000", "").strip()}'
-          f'—{end.replace(":00.000000", "").strip()}@{days_str}')
+  dows = [days[index] for index in sorted(day_list)]
+  time = f'{start.replace(":00 ", "").strip().lower()}—{end.replace(":00 ", "").strip().lower()}'
+  combined = ',@'.join(dows) + f'@{time.replace(" ", "@")}'
+  separate = [f'{d}@{time}' for d in dows] + [''] * (3 - len(dows))
+  return combined, separate
 
 
-def mogrify(input_file):
+def mogrify(input_file, separate_meeting_cols):
   """ Convert an ENROLLMENT-CAPACITY query into a useable format, including GenEd info.
   """
   input_path = Path(input_file)
@@ -116,8 +118,11 @@ def mogrify(input_file):
           continue
         if term is None:
           code, term = term_code(row.term, row.session)
-          m, d, y = row[-1].split('/')
-          file_name = f'Enrollments_{y}-{m}-{d}_{term}.csv'
+          m, d, y = (int(col) for col in row[-1].split('/'))
+          # Have to assume 21st century
+          if y < 100:
+            y += 2000
+          file_name = f'Enrollments_{y}-{int(m):02}-{int(d):02}_{term}.csv'
         if row.class_status not in status_counts.keys():
           status_counts[row.class_status] = 0
         status_counts[row.class_status] += 1
@@ -132,9 +137,9 @@ def mogrify(input_file):
         limit = row.enrollment_capacity
         room = row.facil_id
         mode = row.mode
-        meetings_str = make_meetings_str(row.mtg_start, row.mtg_end,
-                                         [row.mon, row.tues, row.wed, row.thurs,
-                                          row.fri, row.sat, row.sun])
+        combined, separate = make_meetings_str(row.mtg_start, row.mtg_end,
+                                               [row.mon, row.tues, row.wed, row.thurs,
+                                                row.fri, row.sat, row.sun])
         gened_key = course_str.replace('@', ' ').strip()
         if gened_key in geneds.keys():
           gened = geneds[gened_key]
@@ -145,18 +150,28 @@ def mogrify(input_file):
 
         # For spaces in the instructor’s name
         instructor = row.instructor.replace(',', ',@').replace(' ', '@')
-        courses.append(f'{course_str} {title} {class_number} {section:>05} {component} {limit:>4} '
-                       f'{enrollment:>3} {room} {meetings_str} {mode} {instructor} '
-                       f'{gened.rd} {gened.copt}')
+        if separate_meeting_cols:
+          courses.append(f'{course_str} {title} {class_number} {section:>05} {component} {limit:>4}'
+                         f' {enrollment:>3} {room} "{separate[0]}" "{separate[1]}" "{separate[2]}"'
+                         f' {mode} {instructor} {gened.rd} {gened.copt}')
+        else:
+          courses.append(f'{course_str} {title} {class_number} {section:>05} {component} {limit:>4}'
+                         f' {enrollment:>3} {room} {combined} {mode} {instructor}'
+                         f' {gened.rd} {gened.copt}')
   courses.sort(key=lambda course: numeric_part(course[8:14]))
   courses.sort(key=lambda course: course[0:7].strip())
+  print(f'Generating {file_name}')
   with open(file_name, 'w') as outfile:
     writer = csv.writer(outfile)
-    writer.writerow(['Course', 'Title', 'Class #', 'Section', 'Component', 'Limit', 'Enrollment',
-                     'Room', 'Schedule', 'Mode', 'Instructor', 'RD', 'COPT'])
+    if separate_meeting_cols:
+      writer.writerow(['Course', 'Title', 'Class #', 'Section', 'Component', 'Limit', 'Enrollment',
+                       'Room', 'First', 'Second', 'Third', 'Mode', 'Instructor', 'RD', 'COPT'])
+    else:
+      writer.writerow(['Course', 'Title', 'Class #', 'Section', 'Component', 'Limit', 'Enrollment',
+                       'Room', 'Schedule', 'Mode', 'Instructor', 'RD', 'COPT'])
     for course in courses:
       row = course.split()
-      row = [col.replace('@', ' ') for col in row]
+      row = [col.replace('@', ' ').replace('"', '') for col in row]
       writer.writerow(row)
 
   for status, count in status_counts.items():
@@ -169,6 +184,7 @@ if __name__ == '__main__':
   parser = ArgumentParser(description='Transform CUNY enrollment query into something useful.')
   parser.add_argument('-d', '--debug', action='store_true')
   parser.add_argument('-q', '--query_file', default=None)
+  parser.add_argument('-s', '--separate_meeting_columns', action='store_true')
   args = parser.parse_args()
   if args.query_file is None:
     that = None
@@ -180,5 +196,4 @@ if __name__ == '__main__':
   else:
     that = Path(args.query_file)
   print(f'Using {that.name}')
-  mogrify(that)
-
+  mogrify(that, args.separate_meeting_columns)
