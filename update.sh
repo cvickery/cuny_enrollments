5a13ccbf97216c9c2aa56a5dcd571318f14dcd09
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-02-09 21:30:57 -0500

    Update update.sh
    Always mogrify.

--- update.sh
+++ update.sh
@@ -3,8 +3,6 @@
 # Get any new course catalog query results
 export LFTP_PASSWORD=`cat /Users/vickery/.lftpwd`
 /usr/local/bin/lftp -f ./getcunyrc
-if [[ $? != 0 ]]
-then echo 'No new data'
-     exit 1
-else ./mogrify.py
-fi
+
+# (re-)generate latest sheet
+./mogrify.py
