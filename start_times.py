3d6ba308b7cbffeb058c3a308a2de1c18f601a2f
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2020-09-07 22:49:24 -0400

    Update start_times.py
    Hard code for cuny-wide query; add semester and institution to report.

--- start_times.py
+++ start_times.py
@@ -10,10 +10,14 @@ import codecs
 import csv
 from collections import namedtuple, defaultdict
 
+from term_codes import term_code
+
 day_names = ['mon', 'tues', 'wed', 'thurs', 'fri', 'sat', 'sun']
+institutions = ['BAR01', 'BCC01', 'BKL01', 'BMC01', 'CSI01', 'CTY01', 'HOS01', 'HTR01', 'JJC01',
+                'KCC01', 'LAG01', 'LEH01', 'MEC01', 'NCC01', 'NYT01', 'QCC01', 'QNS01', 'YRK01']
 cols = None
 start_times = defaultdict(int)  # key by day of (week, hour)
-with codecs.open('downloads/QCCV_SR_CLASS_ENRL_LOC_TIME_RD-41345176.csv',
+with codecs.open('/Users/vickery/Desktop/QCCV_SR_CLASSES_ALL-41450350.csv',
                  'r', encoding='utf-8', errors='replace') as infile:
     reader = csv.reader(infile)
     for line in reader:
@@ -25,11 +29,19 @@ with codecs.open('downloads/QCCV_SR_CLASS_ENRL_LOC_TIME_RD-41345176.csv',
         Row = namedtuple('Row', cols)
       else:
         row = Row._make(line)
+        if row.institution not in institutions or (row.term != '1209'):
+          continue
+        try:
+          code, name, string = term_code(row.term, row.session)
+        except (ValueError, AssertionError):
+          pass
         for day_name in day_names:
           if line[cols.index(day_name)] and line[cols.index('meeting_time_start')]:
-            start_times[(day_names.index(day_name),
-                         line[cols.index('meeting_time_start')][0:2])] += 1
+            start_times[(string,
+                         row.institution,
+                         day_names.index(day_name),
+                         row.meeting_time_start[0:2])] += 1
 
-print('Day, Hour, Freq')
+print('Term, College, Day, Hour, Freq')
 for key in sorted(start_times.keys()):
-  print(f'{day_names[key[0]]}, {key[1]}, {start_times[key]}')
+  print(f' {key[0]}, {key[1]}, {day_names[key[2]]}, {key[3]}, {start_times[key]}')
