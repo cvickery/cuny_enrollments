c14d8fa79b05ee90415ffe1001ade8c9c6e86dc7
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-01-05 23:29:19 -0500

    Update to_drive.py
    Change location of Google client_id.json

--- to_drive.py
+++ to_drive.py
@@ -53,7 +53,8 @@ SCOPES = 'https://www.googleapis.com/auth/drive'
 store = file.Storage('storage.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('/Users/vickery/Google/client_id.json', scope=SCOPES)
+    flow = client.flow_from_clientsecrets('/Users/vickery/Projects/Google_qc/client_id.json',
+                                          scope=SCOPES)
     creds = tools.run_flow(flow, store, flags)
 service = build('drive', 'v3', http=creds.authorize(Http()))
 
