1d79bef9c21ab83211409780c583567b761ca3fa
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2022-11-08 09:08:20 -0500

    Handle missing transfers-applied files better

--- update_enrollments.sh
+++ update_enrollments.sh
@@ -3,27 +3,26 @@
 # Log this execution
 echo "$0 on `hostname` at `date`"
 
-#  Command line args (lifted from Projects/cuny_curriculum/update_db)
-while [ $# -gt 0 ]
-  do
-    if [[ ( "$1" == "--skip-download") || ( "$1" == "-sd" ) ]]
-      then skip_download=1
-    else
-      echo "Usage: $0 [-sd | --skip_download]"
-      exit 1
-    fi
-    shift
-  done
-
-if [[ ! $skip_download ]]
+if [[ `hostname` =~ cuny.edu ]]
 then
   # Get any new course catalog query results, gened course lists, or session dates.
   export LFTP_PASSWORD=`cat /Users/vickery/.lftpwd`
   /usr/local/bin/lftp -f ./getcunyrc
+else
+  echo Cannot access Tumbleweed from `hostname`
 fi
 
 # Piggyback transfers applied query: move to ../transfers_applied project
-mv downloads/CV_QNS_TRNS_DTL* ../transfers_applied/downloads
+(( num_files = 0 ))
+for file in downloads/CV_QNS_TRNS_DTL*
+do echo $file
+  if [[ $file != 'downloads/CV_QNS_TRNS_DTL*' ]]
+  then
+    mv $file ../transfers_applied/downloads
+    (( num_files += 1 ))
+  fi
+done
+[[ $num_files == 0 ]] && echo 'NO NEW TRANSFERS APPLIED FILES'
 
 # (re-)generate latest enrollment and gened sheets
 ./mogrify.py
