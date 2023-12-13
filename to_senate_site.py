8db9247fb530f04638bb0077584be9aafd229b23
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2023-12-13 08:52:56 -0500

    Remove obsolete/missing Google API code.

--- to_senate_site.py
+++ to_senate_site.py
@@ -28,10 +28,10 @@ import sys
 from pathlib import Path
 import argparse
 
-from apiclient.discovery import build
-from apiclient.http import MediaFileUpload
-from httplib2 import Http
-from oauth2client import file, client, tools
+# from apiclient.discovery import build
+# from apiclient.http import MediaFileUpload
+# from httplib2 import Http
+# from oauth2client import file, client, tools
 
 # Get all the csv files in the new_files directory
 
@@ -58,50 +58,50 @@ for new_file in new_files:
 # Now that QC Google Accounts are gone, the rest of this won’t work.
 exit()
 
-flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+# flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
 
-archive_dir_id = '1wpTfVy7MF4Y7ds1mNfUO2eLxInrMHhmQ'
-latest_enrollments_id = '1g36HsFjtf-_emG_4t36HF7S8O1TMS7AsvYBoPUeIiOY'
-session_dates_id = '1YxcGZuGi5MS8SFSQ2iQ23SpNLVQKRlnvrF2onNkEPT8'
+# archive_dir_id = '1wpTfVy7MF4Y7ds1mNfUO2eLxInrMHhmQ'
+# latest_enrollments_id = '1g36HsFjtf-_emG_4t36HF7S8O1TMS7AsvYBoPUeIiOY'
+# session_dates_id = '1YxcGZuGi5MS8SFSQ2iQ23SpNLVQKRlnvrF2onNkEPT8'
 
-SCOPES = 'https://www.googleapis.com/auth/drive'
-store = file.Storage('storage.json')
-creds = store.get()
-if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('/Users/vickery/Projects/Google_qc/client_id.json',
-                                          scope=SCOPES)
-    creds = tools.run_flow(flow, store, flags)
-service = build('drive', 'v3', http=creds.authorize(Http()))
+# SCOPES = 'https://www.googleapis.com/auth/drive'
+# store = file.Storage('storage.json')
+# creds = store.get()
+# if not creds or creds.invalid:
+#     flow = client.flow_from_clientsecrets('/Users/vickery/Projects/Google_qc/client_id.json',
+#                                           scope=SCOPES)
+#     creds = tools.run_flow(flow, store, flags)
+# service = build('drive', 'v3', http=creds.authorize(Http()))
 
-# Upload each new file to Google Drive
-for new_file in new_files:
-  file_metadata = {
-      'name': new_file.name,
-      'parents': [archive_dir_id],  # id of my Google Drive archives folder
-      'mimeType': 'application/vnd.google-apps.spreadsheet'  # Convert csv to Google Sheet on upload
-  }
-  file_name = f'{from_dir}/{new_file.name}'
-  result = service.files().create(body=file_metadata, media_body=file_name).execute()
-  print(f'Uploaded {new_file.name}')
+# # Upload each new file to Google Drive
+# for new_file in new_files:
+#   file_metadata = {
+#       'name': new_file.name,
+#       'parents': [archive_dir_id],  # id of my Google Drive archives folder
+#       'mimeType': 'application/vnd.google-apps.spreadsheet'  # Convert csv to Google Sheet on upload
+#   }
+#   file_name = f'{from_dir}/{new_file.name}'
+#   result = service.files().create(body=file_metadata, media_body=file_name).execute()
+#   print(f'Uploaded {new_file.name}')
 
-  # If this is an enrollments sheet, update the contents of "latest_enrollments"
-  if 'enrollments' in new_file.name:
-    # File's new content.
-    media_body = MediaFileUpload(new_file.resolve(),
-                                 mimetype='application/vnd.google-apps.spreadsheet',
-                                 resumable=True)
-    # Do the update
-    result = service.files().update(fileId=latest_enrollments_id,
-                                    media_body=media_body).execute()
-    print(f'Updated {result["name"]} from {new_file.name}')
+#   # If this is an enrollments sheet, update the contents of "latest_enrollments"
+#   if 'enrollments' in new_file.name:
+#     # File's new content.
+#     media_body = MediaFileUpload(new_file.resolve(),
+#                                  mimetype='application/vnd.google-apps.spreadsheet',
+#                                  resumable=True)
+#     # Do the update
+#     result = service.files().update(fileId=latest_enrollments_id,
+#                                     media_body=media_body).execute()
+#     print(f'Updated {result["name"]} from {new_file.name}')
 
-  # If this is a session dates sheet, update the contents of "session_dates"
-  if 'session' in new_file.name:
-    # File's new content.
-    media_body = MediaFileUpload(new_file.resolve(),
-                                 mimetype='application/vnd.google-apps.spreadsheet',
-                                 resumable=True)
-    # Do the update
-    result = service.files().update(fileId=session_dates_id,
-                                    media_body=media_body).execute()
-    print(f'Updated {result["name"]} from {new_file.name}')
+#   # If this is a session dates sheet, update the contents of "session_dates"
+#   if 'session' in new_file.name:
+#     # File's new content.
+#     media_body = MediaFileUpload(new_file.resolve(),
+#                                  mimetype='application/vnd.google-apps.spreadsheet',
+#                                  resumable=True)
+#     # Do the update
+#     result = service.files().update(fileId=session_dates_id,
+#                                     media_body=media_body).execute()
+#     print(f'Updated {result["name"]} from {new_file.name}')
